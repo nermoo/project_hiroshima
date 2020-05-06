6063a7955248a6823bec65c0d353df18b32738fc
nermoo <aravindakolitha1998@gmail.com>
2020-05-06 20:57:13 +0530

    login and signup pages were added

--- js/custom.js
+++ js/custom.js
@@ -192,7 +192,7 @@ $(function(){
     $("#navBartoggler").blur(function(event){
         var screenWidth=window.innerWidth;
         if(screenWidth<768){
-            $("#navbarSupportedContent").collapse("hide");
+            $("#navbarSupportedContent").collapse('hide');
         }
     });
 });
