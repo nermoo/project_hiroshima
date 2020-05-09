1103c841ebc3d21213c08373c094a355adc16b2f
nermoo <aravindakolitha1998@gmail.com>
2020-05-09 12:56:56 +0530

    imsge slider need to be fixed

--- js/custom.js
+++ js/custom.js
@@ -189,10 +189,40 @@
 // const sentence='my name'+ person.name +'and my age is' +person.age;
 // console.log(sentence);
 $(function(){
+    
     $("#navBartoggler").blur(function(event){
         var screenWidth=window.innerWidth;
-        if(screenWidth<768){
+        if(screenWidth<992){
             $("#navbarSupportedContent").collapse('hide');
         }
     });
+});
+
+$(function(){
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
 });
