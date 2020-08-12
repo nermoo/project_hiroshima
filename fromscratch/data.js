6b005ca2a76995c7ecd902db90551049926963e1
nermoo <nawarathnaaravinda@gmail.com>
2020-08-12 16:10:29 +0530

    items page dynamically create content using js

---
+++ fromscratch/data.js
@@ -0,0 +1,6 @@
+var shoes=new Object();
+shoes.name="Nike";
+shoes.catogary="sport";
+shoes.colour="red";
+shoes.size=7;
+shoes.desc="Nike yeezy sneaker series."
---
+++ fromscratch/items.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" type="text/css" href="items.css">
+    </head>
+    
+    <body>
+        <div class="card">
+            <h3><a></a></h3>
+            <img/>
+        </div>
+    </body>
